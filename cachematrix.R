81c5848ede1be10d7966d0ce4ff798483ee1901a
rakeshas <rkm.dnb@gmail.com>
2020-09-06 14:40:57 +0530

    Updated R Programming Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -3,16 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-    datamatrix <- NULL
-    setmatrix <- function(y){
+makeCacheMatrix <- function(x = matrix()) {      ## making a cache matrix
+    datamatrix <- NULL     ## initialize the cache matrix 'datamatrix' with NULL value
+    setmatrix <- function(y){    ## defining the method 'setmatrix' to set the value of the matrix
         x <<- y
         datamatrix <<- NULL
     }
-    getmatrix <- function() x
-    setinverse <- function(solve) datamatrix <<-solve
-    getinverse <- function() datamatrix
-    list(setmatrix = setmatrix,
+    getmatrix <- function() x     ## defining the method 'getmatrix' to get the value of the matrix
+    setinverse <- function(solve) datamatrix <<-solve     ## defining the method 'setinverse' to set the value of the inverse
+    getinverse <- function() datamatrix   ## defining the method 'getinverse' to get the value of the inverse
+    list(setmatrix = setmatrix,     ## list of names of all methods defined above
          getmatrix = getmatrix,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -20,14 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {    ## defined function to find the inverse of the matrix using 'cacheSolve'
         ## Return a matrix that is the inverse of 'x'
-        datamatrix <- x$getinverse()
-        if (!is.null(datamatrix)){
+        datamatrix <- x$getinverse()  ## checking the data in the 'datamatrix'
+        if (!is.null(datamatrix)){   ## if the 'datamatrix' is not NULL, then return the result stored as cache 
             message (" getting cached data")
             return(datamatrix)
         }
-        data1 <- x$getmatrix()
+        data1 <- x$getmatrix()   ## if the 'datamatrix' is NULL, then get the value of matrix, set the value matrix, set the value of inverse, get the value of inverse and return the inverse fo the matrix
         datamatrix <- solve(data1, ...)
         x$setinverse (datamatrix)
         datamatrix
